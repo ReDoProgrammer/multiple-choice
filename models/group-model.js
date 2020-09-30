c1f9bee6d400fe85b78cf714b90474fe43f40935
ReDo Programer <redoprogrammer@gmail.com>
2020-09-30 15:58:53 +0700

    set unique into name and meta property in group model and subject model

--- models/group-model.js
+++ models/group-model.js
@@ -2,8 +2,8 @@ const mongoose=require('mongoose');
 const Schema = mongoose.Schema;
 
 const groupSchema = new Schema({
-  name:{type:String,default:''},
-  meta:{type:String,default:''},
+  name:{type:String,default:'',unique:true},
+  meta:{type:String,default:'',unique:true},
   subjects:[{
       type:Schema.Types.ObjectId,
       ref:'subject'
--- models/subject-model.js
+++ models/subject-model.js
@@ -2,8 +2,8 @@ const mongoose=require('mongoose');
 const Schema = mongoose.Schema;
 
 const subjectSchema = new Schema({
-  name:{type:String,default:''},
-  meta:{type:String,default:''},
+  name:{type:String,default:'',unique:true},
+  meta:{type:String,default:'',unique:true},
   group:{
     type: Schema.Types.ObjectId,
     ref:  'group'

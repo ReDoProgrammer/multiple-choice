4c5fd0431a30519fab1b9de26d90c1d9becc4742
ReDo Programer <redoprogrammer@gmail.com>
2020-10-01 19:51:29 +0700

    show avatar in log

--- routes/user/facebook-route.js
+++ routes/user/facebook-route.js
@@ -24,6 +24,7 @@ router.get('/account', ensureAuthenticated, function(req, res) {
         //if facebook account already exist in database
         //asign session user = user found
         req.session.user = user;
+        console.log('avatar: ',req.session.user.avatar);
         res.redirect('/');
       }else{
         User.create({

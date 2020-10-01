c01f3d77025cf5f7b439af62475cbf18d394a520
ReDo Programer <redoprogrammer@gmail.com>
2020-10-01 19:38:08 +0700

    if facebook user not exist in database, create it. then asign session

--- routes/user/facebook-route.js
+++ routes/user/facebook-route.js
@@ -1,5 +1,5 @@
 /*
-  //FACEBOOK AUTHENTICATE
+//FACEBOOK AUTHENTICATE
 */
 const router = require('express').Router();
 const User = require('../../models/user-model');
@@ -9,22 +9,34 @@ FacebookStrategy = require('passport-facebook').Strategy;
 router.get('/auth/facebook',passport.authenticate('facebook'),  function(req, res){
 });
 router.get('/auth/facebook/callback',
-  passport.authenticate('facebook', { failureRedirect: '/' }),
-  function(req, res) {
-    res.redirect('/account');
+passport.authenticate('facebook', { failureRedirect: '/' }),
+function(req, res) {
+  res.redirect('/account');
 });
 
 router.get('/account', ensureAuthenticated, function(req, res) {
-  console.log('account:',req.user);
   let id = req.user.id;
   User.findOne({username:id},function(err,user){
     if(err){
       console.log('find user failed in facebook auth: '+new Error(err));
     }else{
       if(user){
-        console.log(user);
+        //if facebook account already exist in database
+        //asign session user = user found
+        req.session.user = user;
       }else{
-        console.log(id+' not exist');
+        User.create({
+          username:id,
+          fullname:req.user.displayName,
+          avatar:req.user.photos[0].value
+        },function(err,usr){
+          if(err){
+            console.log('create user via facebook user failed: '+new Error(err));
+          }else{
+            req.session.user = usr;
+            res.render('home/index');
+          }
+        });
       }
     }
   });

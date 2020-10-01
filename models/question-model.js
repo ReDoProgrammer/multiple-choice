0cef42920691d8dddc8b6282e0085e2047f95052
ReDo Programer <redoprogrammer@gmail.com>
2020-10-02 03:58:12 +0700

    edit route and view question

--- models/question-model.js
+++ models/question-model.js
@@ -10,6 +10,11 @@ const questionSchema = new Schema({
   option_d:{type:String,default:''},
   answer:{type:String,default:''},
   description:{type:String,default:''},
+  created_by:{
+    type:Schema.Types.ObjectId,
+    ref:'user'
+  },
+  is_actived:{type:Boolean,default:false},
   subject:{type:Schema.Types.ObjectId,ref:'subject'}
 });
 

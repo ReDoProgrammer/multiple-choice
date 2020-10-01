abde315e66875516f4d48f5ddabe125b6f43cabe
ReDo Programer <redoprogrammer@gmail.com>
2020-10-01 19:27:42 +0700

    facebook route check user exist

--- routes/user/facebook-route.js
+++ routes/user/facebook-route.js
@@ -7,19 +7,26 @@ const passport = require('passport');
 FacebookStrategy = require('passport-facebook').Strategy;
 
 router.get('/auth/facebook',passport.authenticate('facebook'),  function(req, res){
-  console.log('/auth/facebook');
-    // The request will be redirected to Facebook for authentication, so this
-    // function will not be called.
 });
 router.get('/auth/facebook/callback',
   passport.authenticate('facebook', { failureRedirect: '/' }),
   function(req, res) {
-    console.log('callback',req.user);
     res.redirect('/account');
 });
 
 router.get('/account', ensureAuthenticated, function(req, res) {
   console.log('account:',req.user);
+  User.findOne({username:req.user.id},function(err,user){
+    if(err){
+      console.log('find user failed in facebook auth: '+new Error(err));
+    }else{
+      if(user){
+        console.log(user);
+      }else{
+        console.log(user.id+' not exist');
+      }
+    }
+  });
 });
 
 function ensureAuthenticated(req, res, next) {

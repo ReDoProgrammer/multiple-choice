42fbea461b145b275180fb749599a6384e12cf99
ReDo Programer <redoprogrammer@gmail.com>
2020-10-03 14:58:28 +0700

    list question with checkbox is actived or not

--- routes/admin/question-route.js
+++ routes/admin/question-route.js
@@ -59,11 +59,13 @@ router.get('/list-by-subject',(req,res)=>{
 });
 
 router.get('/list-with-conditions',(req,res)=>{
-  let page = req.query.page;
-  let subject = req.query.subject;
-  let search = req.query.search;
-  let is_actived = req.query.is_actived;
+  let {page,subject,search,checked} = req.query;
   let pageSize = config.pageSize;
+
+
+  let is_actived = checked=='true'?true:false;
+  // console.log({page,subject,search,is_actived,actived});
+
   Question.find({
     subject:subject,
     question: { $regex: search, $options: "i" },

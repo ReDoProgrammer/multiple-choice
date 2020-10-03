a0b4374c0eac9d49b4237819045528d1fe56d912
ReDo Programer <redoprogrammer@gmail.com>
2020-10-03 13:03:04 +0700

    fix get memcode and current time

--- common/common.js
+++ common/common.js
@@ -2,23 +2,24 @@ var { DateTime } = require('luxon');
 module.exports.MemberCode = ()=>{
 
   let d = DateTime.local();
-  let date = d.c.year<10?'0'+d.c.year:d.c.year;
-  let month = d.c.month<10?'0'+d.c.month:d.c.month;
+  let day = d.c.day;
+  let month = d.c.month;
   let year = d.c.year ;
-  let hour = d.c.hour<10?'0'+d.c.hour:d.c.hour;
-  let minutes = d.c.minute<10?'0'+d.c.minute:d.c.minute;
-  let seconds = d.c.second<10?'0'+d.second:d.second;
-  return  (year+month+date+hour+minutes+seconds);
+  let hour = d.c.hour;
+  let minutes = d.c.minute;
+  let seconds = d.c.second;
+  let millisecond = d.c.millisecond;
+  return year+month+day+hour+minutes+seconds+millisecond;
 }
 
 module.exports.currentTime = ()=>{
   let d = DateTime.local();
-  let date = d.c.year<10?'0'+d.c.year:d.c.year;
+  let day = d.c.day<10?'0'+d.c.day:d.c.day;
   let month = d.c.month<10?'0'+d.c.month:d.c.month;
   let year = d.c.year ;
   let hour = d.c.hour<10?'0'+d.c.hour:d.c.hour;
   let minutes = d.c.minute<10?'0'+d.c.minute:d.c.minute;
   let seconds = d.c.second<10?'0'+d.second:d.second;
 
-  return  date+'/'+month+'/'+year+' '+hour+':'+minutes+':'+seconds;
+  return  day+'/'+month+'/'+year+' '+hour+':'+minutes+':'+seconds;
 }

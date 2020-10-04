4c9d1a3377388de6ad391b011e2519771953539a
ReDo Programer <redoprogrammer@gmail.com>
2020-10-04 20:26:42 +0700

    show subject summary and document summary on home view

--- routes/admin/document-route.js
+++ routes/admin/document-route.js
@@ -3,6 +3,8 @@
 */
 const router = require('express').Router();
 const middleware = require('../../middlewares/middleware');
+const Group = require('../../models/group-model');
+const Subject = require('../../models/subject-model');
 const Document = require('../../models/document-model');
 
 router.get('/',middleware.isAdmin,(req,res)=>{
@@ -36,7 +38,11 @@ router.delete('/delete',middleware.isAdmin,(req,res)=>{
 
 router.get('/list',(req,res)=>{
   let {subject,doctype,search} = req.query;
-  Document.find({})
+  Document.find({
+    subject:subject,
+    doctype:doctype,
+    name: { $regex: search, $options: "i" },
+  })
   .populate('doctype','icon icon_class')
   .populate([{ path: 'subject', populate: { path: 'group' }}])
   .exec(function(err,documents){
@@ -44,6 +50,35 @@ router.get('/list',(req,res)=>{
       console.log('list document failed: '+new Error(err));
     }else{
       res.send({code:200,documents:documents,msg:'load documents successfully'});
+    }
+  });
+});
+
+router.get('/list-by-subject',(req,res)=>{
+  let {group,subject} = req.query;
+  Group.findOne({meta:group},function(err,gr){
+    if(err){
+      console.log('find group in /document/list-by-subject failed: '+new Error(err));
+    }else{
+      if(gr){
+        Subject.findOne({group:gr._id,meta:subject},function(err,sbj){
+          if(err){
+            console.log('find subject failed in document/list-by-subject: '+new Error(err));
+          }else{
+            if(sbj){
+              Document.find({subject:sbj._id})
+              .populate('doctype','icon icon_class')
+              .exec(function(err,documents){
+                if(err){
+                  console.log('find document in list document by subject failed: '+new Error(err));
+                }else{
+                  res.send({code:200,msg:'list document by subject successfully',documents:documents});
+                }
+              });
+            }
+          }
+        });
+      }
     }
   });
 });

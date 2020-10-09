e3164a1a92b5c3fc24d679ddb45eac8c59abf05e
ReDo Programer <redoprogrammer@gmail.com>
2020-10-10 03:16:22 +0700

    ...1

--- server.js
+++ server.js
@@ -161,10 +161,11 @@ io.on('connection',function(socket){
 
     }
 
+    delete members[socket.id];
     //sau khi logout xong thì load lại profile
-    socket.emit('load-profile',null);//load thông tin ở phía tay phải
+    socket.emit('load-profile',1);//load thông tin ở phía tay phải
     // socket.emit('load-chat-controls',{user:user});//load control bình luận ẩn/hiện control gửi bình luận
-    socket.emit('load-top-right',null);//load thông tin tài khoản ở góc phải phía trên view
+    socket.emit('load-top-right',1);//load thông tin tài khoản ở góc phải phía trên view
   });
 
 

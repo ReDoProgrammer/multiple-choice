4929ff7190ff1a2764e4e9b2cdc80ee2e3b666de
ReDo Programer <redoprogrammer@gmail.com>
2020-10-10 01:14:38 +0700

    ..

--- server.js
+++ server.js
@@ -141,9 +141,9 @@ io.on('connection',function(socket){
       if(user.isNew){//nếu là thành viên mới thì cập nhật tổng số thành viên
         io.sockets.emit('total-members');
       }
-      socket.emit('load-profile',{user:user});//load thông tin ở phía tay phải
+      socket.emit('load-profile',user);//load thông tin ở phía tay phải
       // socket.emit('load-chat-controls',{user:user});//load control bình luận ẩn/hiện control gửi bình luận
-      socket.emit('load-top-right',{user:user});//load thông tin tài khoản ở góc phải phía trên view
+      socket.emit('load-top-right',user);//load thông tin tài khoản ở góc phải phía trên view
     });
   }
 

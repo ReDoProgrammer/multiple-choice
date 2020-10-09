7e817daa456cc800993136348f2cdb6f812be013
ReDo Programer <redoprogrammer@gmail.com>
2020-10-09 19:49:21 +0700

    login with facebook in login view

---
+++ public/user/js/social-auth/fbapi.js
@@ -0,0 +1,43 @@
+window.fbAsyncInit = function() {
+  FB.init({
+    appId      : '957295484766781',
+    cookie     : true,
+    xfbml      : true,
+    version    : 'v8.0'
+  });
+
+  FB.getLoginStatus(function(response) {
+    statusChangeCallback(response);
+  });
+
+};
+
+(function(d, s, id){
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) {return;}
+  js = d.createElement(s); js.id = id;
+  js.src = "https://connect.facebook.net/en_US/sdk.js";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
+function statusChangeCallback(response){
+  if(response.status == 'connected'){
+    console.log('logged in and authenticated: ',response);
+    FB.api('/me?fields=name,picture',function(res){
+      console.log(res);
+    })
+  }else{
+    console.log('you are not logged in');
+  }
+}
+
+function checkLoginState() {
+  FB.getLoginStatus(function(response) {
+    statusChangeCallback(response);
+  });
+}
+
+$('#btnFBLogout').click(function(){
+  FB.logout();
+  console.log('you are logged out');
+});

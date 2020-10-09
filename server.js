f4a22e7953fc799968b1b52dfffda7289cb3a2f9
ReDo Programer <redoprogrammer@gmail.com>
2020-10-09 07:48:12 +0700

    set server is realtime server with socket.io

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const config = require('./configs/config');
 const bodyParser = require('body-parser');//body parser dùng để lấy dữ liệu từ form
 const passport = require('passport');
 
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
 
 //phần routes của admin
 const adminRoutes = require('./routes/admin/admin-route');
@@ -125,6 +127,25 @@ mongoose.connect(config.mongodb.mongodb,{
 },()=>{
 	console.log('connect database successfully');
 });
-app.listen(8080,()=>{
-	console.log("app is running on port 8080");
+server.listen(8080,()=>{
+  console.log('server is running');
+});
+var count = 0;
+var $ipsConnected = [];
+io.on('connection',function(socket){
+  var $ipAddress = socket.handshake.address;
+  if (!$ipsConnected.hasOwnProperty($ipAddress)) {
+    $ipsConnected[$ipAddress] = 1;
+    count++;
+    socket.emit('counter', {count:count});
+  }
+  console.log("client is connected"+$ipAddress);
+
+  socket.on('disconnect', function() {
+    if ($ipsConnected.hasOwnProperty($ipAddress)) {
+      delete $ipsConnected[$ipAddress];
+      count--;
+      io.sockets.emit('counter', {count:count});
+    }
+  });
 });

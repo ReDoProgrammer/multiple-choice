1866adacdc7a332011d45ee35274987efb8934af
ReDo Programer <redoprogrammer@gmail.com>
2020-10-10 01:27:07 +0700

    load user information in top-right

--- server.js
+++ server.js
@@ -141,6 +141,7 @@ io.on('connection',function(socket){
       if(user.isNew){//nếu là thành viên mới thì cập nhật tổng số thành viên
         io.sockets.emit('total-members');
       }
+      console.log(user);
       socket.emit('load-profile',user);//load thông tin ở phía tay phải
       // socket.emit('load-chat-controls',{user:user});//load control bình luận ẩn/hiện control gửi bình luận
       socket.emit('load-top-right',user);//load thông tin tài khoản ở góc phải phía trên view

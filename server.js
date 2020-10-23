acb1bda05ce905fa4f3996b3c8f0f755ab5d8ff8
ReDoProgrammer <redoprogrammer@gmail.com>
2020-10-24 02:01:30 +0700

    populate in only row request

--- server.js
+++ server.js
@@ -181,6 +181,7 @@ io.on('connection',function(socket){
   });
 
   socket.on('send-exam',(data)=>{
+    console.log(socket.room_id,data.room);
     io.sockets.in(socket.room_id).emit('populate-questions',data);
   });
 

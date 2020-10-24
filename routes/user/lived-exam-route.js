280c3c0ee26b6712d57ddd4a32b0c3e6cecc35af
ReDoProgrammer <redoprogrammer@gmail.com>
2020-10-25 06:32:02 +0700

    get current local datetime

--- routes/user/lived-exam-route.js
+++ routes/user/lived-exam-route.js
@@ -167,9 +167,8 @@ router.post("/generate-exam", middleware.isLoggedIn, (req, res) => {
 
 //hàm trả về danh sách phòng có trạng thái là -1: chưa thi hoặc 0: đang thi
 router.get("/list", (req, res) => {
-  let d = DateTime.local();
-  let now = DateTime.DateTime;
-  console.log(now);
+  let d = new Date();
+  console.log(d.toLocaleString());
   LivedRoom.find({ status: { $in: [-1, 0] } })
     .populate("subject", "name")
     .exec(function (err, rooms) {

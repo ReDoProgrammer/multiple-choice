c47ded8db1bf6d977ab395d88812ba7e7887a744
ReDoProgrammer <redoprogrammer@gmail.com>
2020-10-26 05:55:37 +0700

    get logged in users that not in any room

--- server.js
+++ server.js
@@ -188,8 +188,6 @@ io.on('connection',function(socket){
     const user = userJoin(socket.id, username,avatar,member_code, room,finished);    
     socket.join(user.room);
     
-    let u = getCurrentUser(socket.id);
-    u.room = user.room;
     // Send users and room info
     io.to(user.room).emit('users-in-room', {
       room: user.room,

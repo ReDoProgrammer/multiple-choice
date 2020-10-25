aec209969be4c930f0ceed1a804939962856c4ef
ReDoProgrammer <redoprogrammer@gmail.com>
2020-10-26 06:10:48 +0700

    get all current users

--- utils/users.js
+++ utils/users.js
@@ -1,41 +1,42 @@
-const users = [];//mảng lưu user
-
+const users = []; //mảng lưu user
 
 //hàm xử lý sự kiện khi người dùng truy cập web
-function userConnect(socket_id){
-  let user = {
-    socket_id
-  }  
-  users.push(user);
+function userConnect(socket_id) {
+  const index = users.findIndex(x => x.socket_id === socket_id);
+  if (index !== -1) {
+    let user = {
+      socket_id,
+    };
+    users.push(user);
+  }
   return users;
 }
 
 //hàm xử lý sự kiện người dùng thoát khỏi trình duyệt web
-function userDisconnect(socket_id){
-  const index = users.findIndex(user => user.socket_id === socket_id);  
+function userDisconnect(socket_id) {
+  const index = users.findIndex((user) => user.socket_id === socket_id);
   if (index !== -1) {
-    return users.splice(index, 1)[0];    
+    return users.splice(index, 1)[0];
   }
-  console.log('all users in array: ',users);
+  console.log("all users in array: ", users);
   return users;
 }
 
-
-function userLoggedIn(user){
-  let chk = users.find(x=>x.username === user.username);
-  if(!chk){
+function userLoggedIn(user) {
+  let chk = users.find((x) => x.username === user.username);
+  if (!chk) {
     users.push(user);
   }
-  console.log('users logged in: ',users);
+  console.log("users logged in: ", users);
   return users;
 }
 
 // Join user to exam room
-function userJoin(socket_id, username,avatar,member_code, room,finished) {
-
-  let chk = users.find(x=>x.socket_id == socket_id && x.room == room);
-  if(!chk){//chỉ add user khi user đó chưa có trong room tương ứng
-    const user = {socket_id, username,avatar,member_code, room ,finished};
+function userJoin(socket_id, username, avatar, member_code, room, finished) {
+  let chk = users.find((x) => x.socket_id == socket_id && x.room == room);
+  if (!chk) {
+    //chỉ add user khi user đó chưa có trong room tương ứng
+    const user = { socket_id, username, avatar, member_code, room, finished };
     users.push(user);
     return user;
   }
@@ -44,25 +45,25 @@ function userJoin(socket_id, username,avatar,member_code, room,finished) {
 
 // Get current user
 function getCurrentUser(socket_id) {
-  return users.find(user => user.socket_id === socket_id);
+  return users.find((user) => user.socket_id === socket_id);
 }
 
 //return all rooms
-function getAllUsers(){
+function getAllUsers() {
   return users;
 }
 
 // User leaves chat
 function userLeave(socket_id) {
-  const index = users.findIndex(user => user.socket_id === socket_id);  
+  const index = users.findIndex((user) => user.socket_id === socket_id);
   if (index !== -1) {
-    return users.splice(index, 1)[0];    
+    return users.splice(index, 1)[0];
   }
 }
 
 // Get room rooms
 function getRoomUsers(room) {
-  return users.filter(user => user.room === room);
+  return users.filter((user) => user.room === room);
 }
 
 module.exports = {
@@ -73,5 +74,5 @@ module.exports = {
   getCurrentUser,
   userLeave,
   getRoomUsers,
-  getAllUsers
+  getAllUsers,
 };

9b067d4d06df544a18e835f2d01c418793c8b368
ReDoProgrammer <redoprogrammer@gmail.com>
2020-10-28 01:30:34 +0700

    special arrays to store visitors and members

--- server.js
+++ server.js
@@ -210,6 +210,7 @@ io.on('connection',function(socket){
     //cập nhật lại danh sách user trong room
     let room = getRoom(socket.id);
     leaveRoom(socket.id);
+    console.log('room:',room);
     io.to(room).emit('users-in-room', {
       room: room,
       users: membersInRoom(room)

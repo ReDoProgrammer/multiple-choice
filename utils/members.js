4248c653b04156229092095555e471cab080e012
ReDoProgrammer <redoprogrammer@gmail.com>
2020-10-28 02:39:12 +0700

    send an invitation

--- utils/members.js
+++ utils/members.js
@@ -23,7 +23,7 @@ function joinRoom(socket_id,room){
         removeMemberBySocketId(tmp.socket_id); 
         pushMember(member);   
     } catch (error) {
-        console.log('join room failed: '+new Error(error),room,'socket:'+socket_id);
+        console.log('member:',getMemberBySocketId(socket_id));
     }
 }
 
